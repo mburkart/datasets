8b441541d3998c77b6bbbb4ddb5e66ebc95655b3
Janek Bechtel <janek.bechtel@student.kit.edu>
2020-02-21 14:12:39 +0100

    adapt while creation

--- make_while.sh
+++ make_while.sh
@@ -1,15 +1,17 @@
 ERA=$1
 
-ls /portal/ekpbms2/home/jbechtel/kappa_skim_workdir/nmssm_skims_${ERA}_*/*/*conf > while.sh
+ls /portal/ekpbms2/home/jbechtel/kappa_skim_workdir/nmssm_skims*/*/*conf > while_${ERA}.sh
 
-sed -i "s/^/go.py /g" while.sh
-sed -i "s/conf/conf -G/g" while.sh
+sed -i "s/^/go.py /g" while_${ERA}.sh
+sed -i "s/conf/conf -G/g" while_${ERA}.sh
 
-sed -i "1 ido" while.sh
-sed -i "1 iwhile [ -f \".lock\" ]" while.sh
-sed -i "1 itouch .lock" while.sh 
+sed -i "1 ido" while_${ERA}.sh
+sed -i "1 iwhile_${ERA} [ -f \".lock\" ]" while_${ERA}.sh
+sed -i "1 itouch .lock" while_${ERA}.sh 
 
-sed -i "\$adone" while.sh
+sed -i "\$adone" while_${ERA}.sh
+
+chmod u+x while_${ERA}.sh
 
 
 
